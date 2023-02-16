31bfa771e55dffe13f2076fe113aa5cf5ba53be6
Phelele <Phelelem2@gmail.com>
2023-02-16 11:33:01 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
- * main - A program that print a line using the printf function
- * Return: 0 (Success)
+ * *Description: main - Prints out a string to stdout
+ * *Return: 0 if success
  */
 
 int main(void)
