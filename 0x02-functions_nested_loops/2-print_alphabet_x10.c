8f2695fe40fc186ec6874eebbee5dc54a19d6f77
Phelele <Phelelem2@gmail.com>
2023-02-21 11:20:03 -0800

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints the alphabets in lowercase ten times
+ */
+
+void print_alphabet_x10(void)
+{
+	int j;
+	int x;
+
+	for (x = 0; x < 10; x++)
+	{
+		for (j = 'a'; j <= 'z'; j++)
+		{
+			_putchar(j);
+		}
+		putchar('\n');
+	}
+}
+
